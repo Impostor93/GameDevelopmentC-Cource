4866a989fb7a1494d4dff5fae7746452e45b6267
Petar <Crow_93@abv.bg>
2015-06-30 19:18:03 +0300

    set up project

--- MyGame/SpaceGame/SpaceGame/tools/editor_tileless/CIndieLib.cpp
+++ MyGame/SpaceGame/SpaceGame/tools/editor_tileless/CIndieLib.cpp
@@ -72,7 +72,7 @@ bool CIndieLib::init() {
 	//_entity3dManager       =   new     IND_Entity3dManager;
 	_math                =   new     IND_Math;
 
-	IND_WindowProperties props ("IndieLib", 800, 600, 32, false, false,true);//example with different numbers, 0, 1, 23, -2333.
+	IND_WindowProperties props ("Genesis", 800, 600, 32, false, 1,false);//example with different numbers, 0, 1, 23, -2333.
 	
 	
 	_window = _render        ->initRenderAndWindow(props);                                 
@@ -122,11 +122,11 @@ void CIndieLib::end() {
 	DISPOSE(_fontManager);
 	DISPOSE(_animationManager);
 	DISPOSE(_surfaceManager);
-    	DISPOSE(_imageManager);
+		DISPOSE(_imageManager);
 	//DISPOSE(_lightManager);
 	DISPOSE(_render);
 
 	IndieLib::end();
 
-    DISPOSE(_pinstance);
+	DISPOSE(_pinstance);
 }
